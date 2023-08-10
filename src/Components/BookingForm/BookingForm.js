17e38314c1a054e5bccf3cd3f8ad1d65779ab7d1
alisobhy22 <64151548+alisobhy22@users.noreply.github.com>
2023-08-10 22:04:44 +0300

    implemnted useReducer

--- src/Components/BookingForm/BookingForm.js
+++ src/Components/BookingForm/BookingForm.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import { useState } from 'react'
-function BookingForm() {
-  const [date, setDate] = useState('')
-  const [time, setTime] = useState('20:00')
-  const [guests, setGuests] = useState(1)
-  const [occasion, setOccasion] = useState('Birthday')
-  const [availabletimes,setAvailabletimes] = useState(['17:00','18:00','19:00','20:00','21:00','22:00','23:00'])
-  function onsubmit(e){
-    e.preventDefault()
-    console.log('submitted')
-    console.log(date)
-    console.log(time)
-    console.log(guests)
-    console.log(occasion)
+function BookingForm(props) {
+
+  const onDateChange = (e) => {
+    props.setDate(e.target.value)
+    const newTimes = props.updateTimes(e.target.value)
+    console.log(newTimes)
+    props.dispatch({type: 'update', payload: newTimes})
   }
   return (
-    <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }} onSubmit={onsubmit}>
+    <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }} onSubmit={props.onsubmit}>
         <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" value={date} onChange={(e)=> setDate(e.target.value)} />
+        <input type="date" id="res-date" value={props.date} onChange={onDateChange} />
 
         <label htmlFor="res-time">Choose time</label>
-        <select id="res-time" value={time}onChange={(e)=> setTime(e.target.value)}>
-            {availabletimes.map((time) => ( <option key={time}>{time}</option>))}
+        <select id="res-time" value={props.time}onChange={(e)=> props.setTime(e.target.value)}>
+            {props.availabletimes.map((time) => ( <option key={time}>{time}</option>))}
         </select>
 
         <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e)=> setGuests(e.target.value)} />
+        <input type="number" placeholder="1" min="1" max="10" id="guests" value={props.guests} onChange={(e)=> props.setGuests(e.target.value)} />
 
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+        <select id="occasion" value={props.occasion} onChange={(e) => props.setOccasion(e.target.value)}>
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
--- src/Pages/BookingPage.js
+++ src/Pages/BookingPage.js
@@ -3,12 +3,76 @@ import Nav from '../Components/Nav/Nav'
 import Footer from '../Components/Footer/Footer'
 import './BookingPage.css'
 import BookingForm from '../Components/BookingForm/BookingForm'
+import { useState, useReducer } from 'react'
+
+const availabletimesReducer = (state, action)=> {
+  switch (action.type) {
+    case 'update':
+      return action.payload
+    case 'initialize':
+      return ['17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+    default:
+      throw new Error()
+  }
+}
+
+const initializeTimes = () => {
+  return ['17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+}
+
+const updateTimes = (selectedDate) => {
+  if (selectedDate === '2023-08-01')
+  {
+    console.log('date is 2023-08-01')
+    return ['17:00','18:00','19:00','20:00','21:00','22:00','23:00','24:00']
+  }
+  else
+  {
+    console.log('date is not 2023-08-01')
+    return ['17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+  }
+};
+
 function BookingPage() {
+
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('20:00')
+  const [guests, setGuests] = useState(1)
+  const [occasion, setOccasion] = useState('Birthday')
+  const [availabletimes, dispatch] = useReducer(availabletimesReducer, initializeTimes())
+
+
+
+  
+
+  const onsubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+    console.log(date)
+    console.log(time)
+    console.log(guests)
+    console.log(occasion)
+  }
+
+
   return (
     <>
         <Nav />
         <div className='Booking-container'>
-            <BookingForm />
+            <BookingForm  
+                date={date}
+                setDate={setDate}
+                time={time}
+                setTime={setTime}
+                guests={guests}
+                setGuests={setGuests}
+                occasion={occasion}
+                setOccasion={setOccasion}
+                availabletimes={availabletimes}
+                dispatch={dispatch}
+                updateTimes={updateTimes}
+                onsubmit={onsubmit}
+            />
         </div>
         <Footer />
     </>
