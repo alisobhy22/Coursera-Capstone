d1dc537202288b3a2fe5052b8f11d9f11aa6d830
alisobhy22 <64151548+alisobhy22@users.noreply.github.com>
2023-08-10 16:03:06 +0300

    Footer done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import './Footer.css'
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div>
+          <h3>Numbers</h3>
+          <p>Phone: 123456789</p>
+          <p>Fax: 123456789</p>
+        </div>
+        <div>
+          <h3>Location</h3>
+          <p>Address: 123456789</p>
+          <p>City: 123456789</p>
+          <p>Country: 123456789</p>
+        </div>
+        <div>
+          <h3>Address</h3>
+          <p>Address: 123456789</p>
+          <p>City: 123456789</p>
+          <p>Country: 123456789</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
